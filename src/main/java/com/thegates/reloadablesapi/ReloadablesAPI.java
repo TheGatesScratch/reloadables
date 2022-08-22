8d5ea5b8851097d0968a3de36b5c3684bde7c702
TheGatesScratch <mtkarels@outlook.com>
2022-08-22 10:12:34 +0200

    Move Transferrer, fix naming

--- src/main/java/com/thegates/reloadablesapi/ReloadablesAPI.java
+++ src/main/java/com/thegates/reloadablesapi/ReloadablesAPI.java
@@ -1,6 +1,55 @@
 package com.thegates.reloadablesapi;
 
+import com.thegates.gatebase.util.ItemUtil;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class ReloadablesAPI extends JavaPlugin {
+
+    private final Map<Class<?>, List<Transferrer<?, ?>>> transferrers = new HashMap<>(0, 0.9f);
+
+    public static <D> Transferrer<ItemStack, D> persistentDataTransferrer(NamespacedKey key, PersistentDataType<?, D> dataType, BiConsumer<ItemMeta, D> action) {
+        return metaTransferrer(itemMeta -> itemMeta.getPersistentDataContainer().get(key, dataType), (m, d) -> m.getPersistentDataContainer().set(key, dataType, d), m -> m.getPersistentDataContainer().remove(key), action);
+    }
+
+    public static <D> Transferrer<ItemStack, D> metaTransferrer(Function<ItemMeta, D> getter, BiConsumer<ItemMeta, D> setter, Consumer<ItemMeta> remover, BiConsumer<ItemMeta, D> action) {
+        return new Transferrer<>(i -> ItemUtil.withMeta(i, getter), (i, d) -> ItemUtil.withMeta(i, (Consumer<ItemMeta>) m -> setter.accept(m, d)), i -> ItemUtil.withMeta(i, remover), (d, i) -> ItemUtil.withMeta(i, (Consumer<ItemMeta>) m -> action.accept(m, d)));
+    }
+
+    public <T, D> Transferrer<T, D> registerTransferrer(Class<T> type, Transferrer<T, D> transferrer) {
+        transferrers.computeIfAbsent(type, c -> new ArrayList<>(1)).add(transferrer);
+        return transferrer;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nullable
+    public <T> List<Transferrer<T, ?>> getTransferrers(Class<T> type) throws ClassCastException {
+        final List<Transferrer<?, ?>> list = transferrers.get(type);
+        if (list == null) return null;
+        try {
+            return list.stream().map(transferrer -> (Transferrer<T, ?>) transferrer).collect(Collectors.toList());
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Something went wrong casting data transferrers: " + e.getMessage());
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public <T> void transfer(Class<T> tClass, T from, T to) {
+        final List<Transferrer<T, ?>> list = getTransferrers(tClass);
+        if (list != null) list.forEach(transferrer -> transferrer.transfer(from, to));
+    }
 }
---
+++ src/main/java/com/thegates/reloadablesapi/Transferrer.java
@@ -0,0 +1,43 @@
+package com.thegates.reloadablesapi;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Transferrer<T, D> {
+
+    private final BiConsumer<T, D> setter;
+    private final Consumer<T> remover;
+    private final BiConsumer<D, T> action;
+    private final Function<T, D> getter;
+
+    public Transferrer(Function<T, D> getter, BiConsumer<T, D> setter, Consumer<T> remover, BiConsumer<D, T> action) {
+        this.setter = setter;
+        this.remover = remover;
+        this.action = action;
+        this.getter = getter;
+    }
+
+    public void apply(T onto, D data) {
+        set(onto, data);
+        action.accept(data, onto);
+    }
+
+    public void remove(T from) {
+        remover.accept(from);
+    }
+
+    public void set(T t, D data) {
+        setter.accept(t, data);
+    }
+
+    public void transfer(T from, T to) {
+        final D data = getter.apply(from);
+        if (data == null) return;
+        apply(to, data);
+    }
+
+    public boolean has(T t) {
+        return getter.apply(t) != null;
+    }
+}
