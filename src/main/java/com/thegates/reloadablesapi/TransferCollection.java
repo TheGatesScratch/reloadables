c66cee180018025488743ccfb214bbe230c1c487
TheGatesScratch <mtkarels@outlook.com>
2022-10-27 14:14:20 +0200

    Use linkedList, add copy constructor, hasAll.

--- src/main/java/com/thegates/reloadablesapi/TransferCollection.java
+++ src/main/java/com/thegates/reloadablesapi/TransferCollection.java
@@ -1,10 +1,20 @@
 package com.thegates.reloadablesapi;
 
-import java.util.ArrayList;
+import com.thegates.gatebase.GateBase;
+
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class TransferCollection<T> {
-    private final Collection<Transferrer<T, ?>> transferrers = new ArrayList<>();
+    private final Collection<Transferrer<T, ?>> transferrers = new LinkedList<>();
+
+    public TransferCollection() {
+    }
+
+    public TransferCollection(TransferCollection<T> other) {
+        this();
+        transferrers.addAll(other.transferrers);
+    }
 
     public <D> Transferrer<T, D> add(Transferrer<T, D> transferrer) {
         transferrers.add(transferrer);
@@ -13,5 +23,9 @@ public class TransferCollection<T> {
 
     public void transfer(T from, T to) {
         transferrers.forEach(t -> t.transfer(from, to));
+    }
+
+    public void hasAll(T toCheck) {
+        GateBase.forEachAND(transferrers, t -> t.has(toCheck));
     }
 }
