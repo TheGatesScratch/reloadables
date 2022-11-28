3214a2ab57b853460c9c60b2b58e642dc0c0d8da
TheGatesScratch <mtkarels@outlook.com>
2022-11-28 21:48:15 +0100

    Util methods use ItemStack -> ItemMeta. Add global reloadables.

--- src/main/java/com/thegates/reloadablesapi/ReloadablesAPI.java
+++ src/main/java/com/thegates/reloadablesapi/ReloadablesAPI.java
@@ -1,31 +1,38 @@
 package com.thegates.reloadablesapi;
 
-import com.thegates.gatebase.util.ItemUtil;
 import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public class ReloadablesAPI extends JavaPlugin {
 
-    public static <D> SingleTransferrer<ItemStack, D> persistentDataTransferrer(NamespacedKey key, PersistentDataType<?, D> dataType, BiConsumer<ItemMeta, D> action) {
-        return metaTransferrer(new SingleTransferrer<>(m -> {
+    private final List<Reloadable> globalReloadables = new ArrayList<>();
+
+    public static <D> SingleTransferrer<ItemMeta, D> persistentDataTransferrer(NamespacedKey key, PersistentDataType<?, D> dataType, BiConsumer<ItemMeta, D> action) {
+        return new SingleTransferrer<>(m -> {
             final var c = m.getPersistentDataContainer();
             if (c.isEmpty()) return null;
             return c.get(key, dataType);
-        }, (m, d) -> m.getPersistentDataContainer().set(key, dataType, d), m -> m.getPersistentDataContainer().remove(key), action));
+        }, (m, d) -> m.getPersistentDataContainer().set(key, dataType, d), m -> m.getPersistentDataContainer().remove(key), action);
     }
 
-    public static <D> SingleTransferrer<ItemStack, D> persistentDataTransferrer(NamespacedKey key, PersistentDataType<?, D> dataType) {
+    public static <D> SingleTransferrer<ItemMeta, D> persistentDataTransferrer(NamespacedKey key, PersistentDataType<?, D> dataType) {
         return persistentDataTransferrer(key, dataType, null);
     }
 
-    public static <D> SingleTransferrer<ItemStack, D> metaTransferrer(SingleTransferrer<ItemMeta, D> transferrer) {
-        return new SingleTransferrer<>(i -> ItemUtil.withMeta(i, transferrer::get), (i, d) -> ItemUtil.withMeta(i, (Consumer<ItemMeta>) m -> transferrer.set(m, d)), i -> ItemUtil.withMeta(i, transferrer::remove), (i, d) -> ItemUtil.withMeta(i, (Consumer<ItemMeta>) m -> transferrer.apply(m, d)));
+    public void addReloadable(Reloadable reloadable) {
+        globalReloadables.add(reloadable);
+    }
+
+    public synchronized void reloadAll() {
+        for (final Reloadable reloadable : globalReloadables) {
+            reloadable.reload();
+        }
     }
 
     @Override
