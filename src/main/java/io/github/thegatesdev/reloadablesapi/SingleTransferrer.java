4e8068795ebc8f960158665df926deebdc4a0ce1
TheGatesScratch <mtkarels@outlook.com>
2022-12-30 17:49:12 +0100

    Tone down to interface.

--- src/main/java/io/github/thegatesdev/reloadablesapi/SingleTransferrer.java
+++ src/main/java/io/github/thegatesdev/reloadablesapi/SingleTransferrer.java
@@ -1,51 +1,12 @@
 package io.github.thegatesdev.reloadablesapi;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
+public interface SingleTransferrer<T, D> extends Transferrer<T> {
+    D get(T t);
 
-public class SingleTransferrer<T, D> implements Transferrer<T> {
+    <E extends T> E apply(E onto, D data);
 
-    private final BiConsumer<T, D> setter;
-    private final Consumer<T> remover;
-    private final BiConsumer<T, D> action;
-    private final Function<T, D> getter;
-
-    public SingleTransferrer(Function<T, D> getter, BiConsumer<T, D> setter, Consumer<T> remover) {
-        this(getter, setter, remover, null);
-    }
-
-    public SingleTransferrer(Function<T, D> getter, BiConsumer<T, D> setter, Consumer<T> remover, BiConsumer<T, D> action) {
-        this.setter = setter;
-        this.remover = remover;
-        this.action = action;
-        this.getter = getter;
-    }
-
-    public D get(T t) {
-        if (t == null) return null;
-        return getter.apply(t);
-    }
-
-    public T apply(T onto, D data) {
-        set(onto, data);
-        if (action != null)
-            action.accept(onto, data);
-        return onto;
-    }
-
-    void set(T t, D data) {
-        setter.accept(t, data);
-    }
-
-    public T transfer(T from, T to) {
-        final D data = getter.apply(from);
-        if (data != null) apply(to, data);
-        return to;
-    }
-
-    public T remove(T from) {
-        remover.accept(from);
-        return from;
+    @Override
+    default <E extends T> E transfer(T from, E to) {
+        return apply(to, get(from));
     }
 }
